74d6e3794b236f44927f985c8c0c89847ad173d1
TheMarsUltor <clorudodepotasio@gmail.com>
2025-04-29 19:42:31 -0300

    Modificacion de SP_LOGOUT
    El update tenia mal el nombre del campo

--- database_queries/sessions/logout.sql
+++ database_queries/sessions/logout.sql
@@ -2,7 +2,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_LOGOUT`(pTOKEN varchar(512))
 BEGIN
 
 update	sessions
-set		finalizado = 1
-where	session_token = pTOKEN;
+set		  finalizada = 1
+where	  session_token = pTOKEN;
 
 END
